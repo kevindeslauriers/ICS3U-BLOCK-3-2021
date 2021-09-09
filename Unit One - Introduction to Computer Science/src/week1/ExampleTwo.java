9b092a3cffee5793b12c6279fa59fac63834a164
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2021-09-09 13:15:03 -0400

    Unit One Initial Commit

---
+++ Unit One - Introduction to Computer Science/src/week1/ExampleTwo.java
@@ -0,0 +1,15 @@
+package week1;
+
+public class ExampleTwo {
+  public static void main(String[] args) {
+    System.out.println("A"); // PRINTS A and hits enter
+    System.out.print("B"); // PRINTS B
+    System.out.print("C"); // PRINTS C
+    System.out.println("D"); // PRINTS D and hits enter
+    System.out.println("E"); // PRINTS E and hits enter
+
+    /**
+     * A BCD E
+     */
+  }
+}
