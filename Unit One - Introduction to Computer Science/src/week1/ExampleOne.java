543f5af7a1adbea2f72de5c535c47d7ad837940b
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2021-09-09 13:17:53 -0400

    Test commit

--- Unit One - Introduction to Computer Science/src/week1/ExampleOne.java
+++ Unit One - Introduction to Computer Science/src/week1/ExampleOne.java
@@ -2,9 +2,9 @@ package week1;
 
 public class ExampleOne {
   public static void main(String[] args) {
-    System.out.println("Welcome to ICS3U - AP"); // System.out.println displays text to the screen and hits enter
-    System.out.println("Welcome to ICS3U - AP");
-    System.out.println("Welcome to ICS3U - AP");
+    System.out.println("Welcome to ICS3U - AP!"); // System.out.println displays text to the screen and hits enter
+    System.out.println("Welcome to ICS3U - AP!");
+    System.out.println("Welcome to ICS3U - AP!");
   }
 
 }
