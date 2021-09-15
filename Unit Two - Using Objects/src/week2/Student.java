41acbbc8d10b7583bb5a03fb4503de83d8bdfc0b
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2021-09-15 13:29:43 -0400

    Added comments

--- Unit Two - Using Objects/src/week2/Student.java
+++ Unit Two - Using Objects/src/week2/Student.java
@@ -8,16 +8,32 @@ package week2;
  */
 public class Student {
 
+  /**
+   * Attributes that define a student The state of a specific student is what is
+   * in the attributes at a specific point
+   */
   private String name;
   private String studentNumber;
   private int grade;
 
+  /**
+   * 
+   * The method with the same name as the class is used to instantiate (create) a
+   * Student Object
+   * 
+   * Constructor - same name as the class and its purpose is to instantiate a
+   * instance of the class (creates a state and places it in its initial state)
+   */
   public Student(String name, String studentNumber, int grade) {
     this.name = name;
     this.studentNumber = studentNumber;
     this.grade = grade;
   }
 
+  /**
+   * displayName, displayStudentNumber, increaseGrade, displayGrade The methods
+   * (actions) in a class define behaviour for the class
+   */
   public void displayName() {
     System.out.println(name);
   }
