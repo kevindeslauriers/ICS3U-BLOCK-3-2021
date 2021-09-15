e1613a77a8098237cbce08c79a621f8fb70d57b1
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2021-09-15 13:43:24 -0400

    StudentExample

---
+++ Unit Two - Using Objects/src/week2/StudentExample.java
@@ -0,0 +1,9 @@
+package week2;
+
+public class StudentExample {
+  public static void main(String[] args) {
+    // ritchie is a vcraiable to an instantce of the class Student
+    // ritchie is an Object (instances of a class)
+    Student ritchie = new Student("Ritchie", "45646", 12);
+  }
+}
