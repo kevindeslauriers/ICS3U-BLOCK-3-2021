ba3a289f8fd2770a6a3d0a0b386414361ed8c518
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2021-09-27 13:32:44 -0400

    BVG Pools 1/2 Solution.

--- Unit Two - Using Objects/src/week4/BayviewGlenPools.java
+++ Unit Two - Using Objects/src/week4/BayviewGlenPools.java
@@ -18,11 +18,31 @@ public class BayviewGlenPools {
 
    private static double getVolume(int length, int width, int shallowHeight, int shallowLength, int transition,
          int deepHeight) {
+      double transitionBase = Math.sqrt(Math.pow(transition, 2) - Math.pow(deepHeight - shallowHeight, 2));
+      double deepLength = length - shallowLength - transitionBase;
+
+      double deepVolume = deepHeight * deepLength * width;
+      double shallowVolume = shallowHeight * shallowLength * width;
+
+      double transitionVolume = transitionBase * shallowHeight * width;
+      transitionVolume += width * (deepHeight - shallowHeight) * transitionBase * 0.5;
+
+      return transitionVolume + shallowVolume + deepVolume;
 
    }
 
    private static double getSurfaceArea(int length, int width, int shallowHeight, int shallowLength, int transition,
          int deepHeight) {
 
+      double transitionBase = Math.sqrt(Math.pow(transition, 2) - Math.pow(deepHeight - shallowHeight, 2));
+      double deepLength = length - shallowLength - transitionBase;
+
+      int shallowBack = shallowHeight * width;
+      int deepBack = deepHeight * width;
+      double bottom = (deepLength + transition + shallowLength) * width;
+      double side = shallowHeight * shallowLength + deepHeight * deepLength + transitionBase * shallowHeight
+            + (deepHeight - shallowHeight) * transitionBase * 0.5;
+
+      return shallowBack + deepBack + bottom + side * 2;
    }
 }
