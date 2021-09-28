24dae65d8b4f65f7fac6f0283ab751af66e385eb
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2021-09-28 12:29:14 -0400

    Formatted BVG Pools

--- Unit Two - Using Objects/src/week4/BayviewGlenPools.java
+++ Unit Two - Using Objects/src/week4/BayviewGlenPools.java
@@ -1,48 +1,52 @@
 package week4;
 
 public class BayviewGlenPools {
-   public static void main(String[] args) {
-      final int length = 20;
-      final int width = 8;
-      final int shallowLength = 5;
-      final int transition = 7;
-      final int shallowHeight = 3;
-      final int deepHeight = 8;
+      public static void main(String[] args) {
+            final int length = 20; // l
+            final int width = 8; // w
+            final int shallowLength = 5; // l1
+            final int transition = 7; // l3
+            final int shallowHeight = 3; // h1
+            final int deepHeight = 8; // h2
 
-      final int linerCost = 2;
+            final int linerCost = 2;
 
-      double surfaceArea = getSurfaceArea(length, width, shallowHeight, shallowLength, transition, deepHeight);
-      double volume = getVolume(length, width, shallowHeight, shallowLength, transition, deepHeight);
+            double surfaceArea = getSurfaceArea(length, width, shallowHeight, shallowLength, transition, deepHeight);
+            double volume = getVolume(length, width, shallowHeight, shallowLength, transition, deepHeight);
 
-   }
+            System.out.println("Volume: " + (int) (volume * 0.9 * 100) / 100.0);
+            System.out.println("Surface Area: " + (int) (surfaceArea * 100) / 100.0);
+            System.out.println("Cost of Liner: $" + (int) ((surfaceArea * linerCost) * 100) / 100.0);
 
-   private static double getVolume(int length, int width, int shallowHeight, int shallowLength, int transition,
-         int deepHeight) {
-      double transitionBase = Math.sqrt(Math.pow(transition, 2) - Math.pow(deepHeight - shallowHeight, 2));
-      double deepLength = length - shallowLength - transitionBase;
+      }
 
-      double deepVolume = deepHeight * deepLength * width;
-      double shallowVolume = shallowHeight * shallowLength * width;
+      private static double getVolume(int length, int width, int shallowHeight, int shallowLength, int transition,
+                  int deepHeight) {
+            double transitionBase = Math.sqrt(Math.pow(transition, 2) - Math.pow(deepHeight - shallowHeight, 2));
+            double deepLength = length - shallowLength - transitionBase;
 
-      double transitionVolume = transitionBase * shallowHeight * width;
-      transitionVolume += width * (deepHeight - shallowHeight) * transitionBase * 0.5;
+            double deepVolume = deepHeight * deepLength * width;
+            double shallowVolume = shallowHeight * shallowLength * width;
 
-      return transitionVolume + shallowVolume + deepVolume;
+            double transitionVolume = transitionBase * shallowHeight * width;
+            transitionVolume += width * (deepHeight - shallowHeight) * transitionBase * 0.5;
 
-   }
+            return transitionVolume + shallowVolume + deepVolume;
 
-   private static double getSurfaceArea(int length, int width, int shallowHeight, int shallowLength, int transition,
-         int deepHeight) {
+      }
 
-      double transitionBase = Math.sqrt(Math.pow(transition, 2) - Math.pow(deepHeight - shallowHeight, 2));
-      double deepLength = length - shallowLength - transitionBase;
+      private static double getSurfaceArea(int length, int width, int shallowHeight, int shallowLength, int transition,
+                  int deepHeight) {
 
-      int shallowBack = shallowHeight * width;
-      int deepBack = deepHeight * width;
-      double bottom = (deepLength + transition + shallowLength) * width;
-      double side = shallowHeight * shallowLength + deepHeight * deepLength + transitionBase * shallowHeight
-            + (deepHeight - shallowHeight) * transitionBase * 0.5;
+            double transitionBase = Math.sqrt(Math.pow(transition, 2) - Math.pow(deepHeight - shallowHeight, 2));
+            double deepLength = length - shallowLength - transitionBase;
 
-      return shallowBack + deepBack + bottom + side * 2;
-   }
+            int shallowBack = shallowHeight * width;
+            int deepBack = deepHeight * width;
+            double bottom = (deepLength + transition + shallowLength) * width;
+            double side = shallowHeight * shallowLength + deepHeight * deepLength + transitionBase * shallowHeight
+                        + (deepHeight - shallowHeight) * transitionBase * 0.5;
+
+            return shallowBack + deepBack + bottom + side * 2;
+      }
 }
