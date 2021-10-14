0479da2a0bf6855cfe93e9c69a63afe77f8e6373
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2021-10-14 12:55:43 -0400

    FRQ Unit 4 Practice

---
+++ Unit Three and Four - Selection and Iteration/src/week6/CoinGame.java
@@ -0,0 +1,36 @@
+package week6;
+
+public class CoinGame {
+
+   private int startingCoins; // starting number of coins
+   private int maxRounds; // maximum number of rounds played
+
+   public CoinGame(int s, int r) {
+      startingCoins = s;
+      maxRounds = r;
+   }
+
+   /**
+    * Returns the number of coins (1, 2, or 3) that player 1 will spend.
+    */
+   public int getPlayer1Move() {
+
+   }
+
+   /**
+    * Returns the number of coins (1, 2, or 3) that player 2 will spend, as
+    * described in part (a).
+    */
+   public int getPlayer2Move(int round) {
+
+   }
+
+   /**
+    * Plays a simulated game between two players, as described in part (b).
+    * 
+    */
+   public void playGame() {
+
+   }
+
+}
---
+++ Unit Three and Four - Selection and Iteration/src/week6/FRQ1.java
@@ -0,0 +1,15 @@
+package week6;
+
+/**
+ * FRQ1
+ */
+public class FRQ1 {
+
+   public static void main(String[] args) {
+      longestStreak("CCAAAAATTT!");
+   }
+
+   private static void longestStreak(String str) {
+
+   }
+}
