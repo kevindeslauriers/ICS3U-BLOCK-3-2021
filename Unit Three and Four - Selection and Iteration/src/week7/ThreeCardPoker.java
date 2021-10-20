0f8f7edde88c6c20d37140842418b83511e15f06
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2021-10-20 12:23:26 -0400

    Discard Function

--- Unit Three and Four - Selection and Iteration/src/week7/ThreeCardPoker.java
+++ Unit Three and Four - Selection and Iteration/src/week7/ThreeCardPoker.java
@@ -33,21 +33,6 @@ public class ThreeCardPoker {
 
    }
 
-   private static String discard(Scanner in, String playerHand) {
-      // ex playerHand = "6H 2D AH"
-
-      // ask the user how many cards to discard
-      // 0 - do nothing
-      // 3 - get them 3 cards to replace there old cards
-      // 1 and 2 => ask them the cards to replace.
-      // ex. if 1 card then user will type 6H if they do not have a 6H it will let the
-      // m know
-      // ex if 2 cards then user will type 6H AD
-
-      // return their new hand
-
-   }
-
    private static String getCard(String usedCards) {
       String card = getFace() + getSuit();
 
@@ -107,5 +92,106 @@ public class ThreeCardPoker {
       }
 
       return bet;
+   }
+
+   private static String discard(Scanner in, String playerHand) {
+      int numCardsToReplace = getNumberToDiscard(in);
+      String temp = playerHand;
+      if (numCardsToReplace == 0)
+         return playerHand;
+      else if (numCardsToReplace == 3) {
+         playerHand = getCard(playerHand + temp) + " ";
+         playerHand += getCard(playerHand + temp) + " ";
+         playerHand += getCard(playerHand + temp) + " ";
+      } else {
+         String cardToReplace = getCardsToDiscard(in, numCardsToReplace, playerHand);
+         int space = cardToReplace.indexOf(" ");
+         String card1 = "";
+         if (space == -1)
+            card1 = cardToReplace;
+         else
+            card1 = cardToReplace.substring(0, space);
+
+         String card = getCard(playerHand + temp);
+         playerHand = playerHand.replace(card1, card);
+         if (numCardsToReplace == 2) {
+            String card2 = cardToReplace.substring(space + 1);
+            card = getCard(playerHand + cardToReplace + temp);
+            playerHand = playerHand.replace(card2, card);
+         }
+      }
+      return playerHand;
+   }
+
+   private static int getNumberToDiscard(Scanner in) {
+      boolean validInput = false;
+      int val = 0;
+      while (!validInput) {
+         System.out.print("Hoe many cards to discard [0,3]: ");
+         try {
+            val = Integer.parseInt(in.nextLine());
+
+            if (val < 0 || val > 3) {
+               System.out.println("Invalid input!");
+            } else {
+               validInput = true;
+            }
+         } catch (Exception ex) {
+            System.out.println("Invalid input!");
+         }
+      }
+      return val;
+   }
+
+   private static String getCardsToDiscard(Scanner in, int numCardsToReplace, String playerHand) {
+      final String VALID_CARDS = "AS2S3S4S5S6S7S8S9S10SJSQSKSAC2C3C4C5C6C7C8C9C10CJCQCKCAD2D3D4D5D6D7D8D9D10DJDQDKDAH2H3H4H5H6H7H8H9H10HJHQHKH";
+      boolean validInput = false;
+      String cards = "";
+      while (!validInput) {
+         System.out.println("Please enter the cards to replace: (ex. 7H KD): ");
+         cards = in.nextLine().toUpperCase();
+         if (countOccurences(cards, " ") != numCardsToReplace - 1) {
+            System.out.println("You must choose " + numCardsToReplace + " cards.");
+         } else if ((numCardsToReplace == 1) && (playerHand.indexOf(cards) < 0))
+            System.out.println("You don't have a " + cards);
+         else if ((numCardsToReplace == 1) && (playerHand.indexOf(cards) >= 0))
+            validInput = true;
+         else if (numCardsToReplace == 2) {
+            int space = cards.indexOf(" ");
+
+            String card1 = cards.substring(0, space);
+            String card2 = cards.substring(space + 1);
+            if (VALID_CARDS.indexOf(card1) < 0)
+               System.out.println("Not a valid card: " + card1);
+            else if (VALID_CARDS.indexOf(card2) < 0)
+               System.out.println("Not a valid card: " + card2);
+            else if (playerHand.indexOf(card1) < 0)
+               System.out.println("You don't have a " + card1);
+            else if (playerHand.indexOf(card2) < 0)
+               System.out.println("You don't have a " + card2);
+            else if (playerHand.equals(card2))
+               System.out.println("You cannot discard the same card.");
+            else
+               validInput = true;
+         }
+
+      }
+
+      return cards;
+
+   }
+
+   private static int countOccurences(String str, String str2) {
+      int count = 0;
+      if (str2.length() > str.length())
+         return 0;
+
+      for (int i = 0; i < str.length(); i++) {
+         String substr = str.substring(i, i + str2.length());
+         if (str2.equals(substr))
+            count++;
+      }
+      return count;
+
    }
 }
