2ce9fec15250dd130eaa7ec3d579a9e5a8d9db32
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2021-11-03 15:10:29 -0400

    Updated DeluxeMeal

--- Unit Five and Nine - Creating Classes and Inheritance/src/week9/DeluxeMeal.java
+++ Unit Five and Nine - Creating Classes and Inheritance/src/week9/DeluxeMeal.java
@@ -5,7 +5,7 @@ public class DeluxeMeal extends Meal {
    private String drink;
 
    public DeluxeMeal(String entree, String sideDish, String drink, double cost) {
-      super(entree, cost);
+      super(entree, cost + 3);
       this.sideDish = sideDish;
       this.drink = drink;
    }
