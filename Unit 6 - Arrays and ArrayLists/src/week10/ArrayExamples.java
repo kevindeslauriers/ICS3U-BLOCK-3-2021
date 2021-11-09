ccc034fe6e7bdb4b3a73b427e5fb0a0e397ee669
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2021-11-09 13:07:50 -0500

    Iterate an Array

--- Unit 6 - Arrays and ArrayLists/src/week10/ArrayExamples.java
+++ Unit 6 - Arrays and ArrayLists/src/week10/ArrayExamples.java
@@ -3,8 +3,30 @@ package week10;
 public class ArrayExamples {
    public static void main(String[] args) {
       primitiveArray();
-      arrayObjectExample();
+      // arrayObjectExample();
+      iterateArrayExample();
 
+      int[] arr = { 5, 3, 2, 6, 3, 3 };
+      mystery(arr);
+
+   }
+
+   private static void mystery(int[] arr) {
+      int index = (int) (Math.random() * arr.length);
+
+      arr[index] = -arr[index];
+   }
+
+   private static void iterateArrayExample() {
+      int[] arr = { 5, 3, 2, 6, 3, 3 };
+
+      int numOdd = 0;
+      for (int i = 0; i < arr.length; i++) {
+         if (arr[i] % 2 == 1)
+            numOdd++;
+      }
+
+      System.out.println(numOdd);
    }
 
    private static void arrayObjectExample() {
