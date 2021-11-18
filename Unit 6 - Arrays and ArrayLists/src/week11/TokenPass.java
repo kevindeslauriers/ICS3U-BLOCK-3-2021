f386a4331d8591fb873c8952b012d03b0383f7be
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2021-11-18 12:45:28 -0500

    TokenPass Solution

--- Unit 6 - Arrays and ArrayLists/src/week11/TokenPass.java
+++ Unit 6 - Arrays and ArrayLists/src/week11/TokenPass.java
@@ -6,13 +6,31 @@ public class TokenPass {
 
    // Part (A)
    public TokenPass(int playerCount) {
-     
+      currentPlayer = (int) (Math.random() * playerCount);
+      tokens = new int[playerCount];
+
+      for (int i = 0; i < tokens.length; i++) {
+         // for (int token : tokens) {
+         tokens[i] = (int) (Math.random() * 10) + 1;
+      }
 
    }
 
    // Part (B)
    public void distributeCurrentPlayerTokens() {
-      
+      int tokensLeft = tokens[currentPlayer];
+      tokens[currentPlayer] = 0;
+
+      int nextPlayer = currentPlayer + 1;
+
+      while (tokensLeft > 0) {
+         if (nextPlayer == tokens.length)
+            nextPlayer = 0;
+
+         tokens[nextPlayer]++;
+         tokensLeft--;
+         nextPlayer++;
+      }
    }
 
 }
